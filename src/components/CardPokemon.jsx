c0a8f3f51d2d2bc7eee165d537ce5994f82aad19
LangerTwo <adrangel95@gmail.com>
2024-04-11 15:28:43 -0500

    algunos cambios

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {primerMayuscula} from '../helper/helpers.js'
 
 const CardPokemon = ({pokemon}) => {
-  console.log(pokemon.sprites.other.dream_world.front_default)
   return (
     <Link to={`/pokemon/${pokemon.id}`} className='card-pokemon'>
         <div className='card-img'>
@@ -13,7 +13,7 @@ const CardPokemon = ({pokemon}) => {
         </div>
         <div className='card-info'>
           <span className='pokemon-id'>N° {pokemon.id}</span>
-          <h3>{pokemon.name}</h3>
+          <h3>{primerMayuscula(pokemon.name)}</h3>
           <div className='card-types'>
             {pokemon.types.map(type => (
               <span key={type.type.name} className={type.type.name}>
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { tailspin } from 'ldrs'
 
 const Loader = () => {
+  tailspin.register()
+
   return (
-    <div>
-      
-    </div>
+   <div className='container-loader'>
+     <l-tailspin
+      size="100"
+      stroke="5"
+      speed="0.9" 
+      color="#14B2D2" 
+    ></l-tailspin>
+   </div>
   )
 }
 
 export default Loader
+
+
+
+
+// Default values shown
+
